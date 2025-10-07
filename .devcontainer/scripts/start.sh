105bc74cc019ee64b5db03cba658d497d512988b
Gerson <43101291+antillgrp@users.noreply.github.com>
2025-10-07 02:46:27 +0000

    fixed

--- .devcontainer/scripts/start.sh
+++ .devcontainer/scripts/start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 export AGENT_VERSION=2.218.1
-cd /home/vscode/azure-pipelines                                                                           && 
- https://download.agent.dev.azure.com/agent/${AGENT_VERSION}/vsts-agent-linux-x64-${AGENT_VERSION}.tar.gz && 
- tar xzf /home/vscode/azure-pipelines/vsts-agent-linux-x64-${AGENT_VERSION}.tar.gz                        && 
- sudo /home/vscode/azure-pipelines/bin/installdependencies.sh
+cd /home/vscode/azure-pipelines                                                                                     && 
+curl -O -L https://download.agent.dev.azure.com/agent/${AGENT_VERSION}/vsts-agent-linux-x64-${AGENT_VERSION}.tar.gz && 
+tar xzf /home/vscode/azure-pipelines/vsts-agent-linux-x64-${AGENT_VERSION}.tar.gz                                   && 
+sudo /home/vscode/azure-pipelines/bin/installdependencies.sh
 
 ADO_ORG=$ADO_ORG
 ADO_PAT=$ADO_PAT
@@ -20,6 +20,7 @@ USER_NAME_LABEL=$( (git config --get user.name) | sed -e 's/ //g')
 # !!!Ignore sensitive tokens from capabilities!!!
 export VSO_AGENT_IGNORE=ADO_PAT,GH_TOKEN,GITHUB_CODESPACE_TOKEN,GITHUB_TOKEN
 
+[ -f /home/vscode/azure-pipelines/config.sh ]        && 
 /home/vscode/azure-pipelines/config.sh --unattended \
 --agent "${AGENT_NAME}" \
 --url "${ADO_URL}" \
@@ -28,4 +29,5 @@ export VSO_AGENT_IGNORE=ADO_PAT,GH_TOKEN,GITHUB_CODESPACE_TOKEN,GITHUB_TOKEN
 --pool "${ADO_POOL_NAME}" \
 --acceptTeeEula
 
-/home/vscode/azure-pipelines/run.sh
+[ -f /home/vscode/azure-pipelines/run.sh ] && 
+/home/vscode/azure-pipelines/run.sh > agent-run.log &
